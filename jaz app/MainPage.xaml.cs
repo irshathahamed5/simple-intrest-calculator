aaa883729e513acec421fb310d5c769a7879cdd0
irshathahamed5 <98414958+irshathahamed5@users.noreply.github.com>
2022-04-27 17:10:20 +0530

    app

---
+++ jaz app/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace jaz_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked_Calculate(object sender,EventArgs e)
+        {
+            double total = Convert.ToDouble(principle.Text) + Convert.ToDouble(principle.Text) * Convert.ToDouble(Months.Text) * Convert.ToDouble(intrest.Text) / 100;
+            llltotal.Text = total.ToString();
+        }
+
+    }
+}
